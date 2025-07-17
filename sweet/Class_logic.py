38fe643444f29f2cfa2f3abdc305d34d90c778e9
Aditya vishwakarma santosh <adityavishwakarma3500@gmail.com>
2025-07-17 11:40:31 +0530

    Imaplement Purchasing a sweet and reducing quantity feature

--- sweet/Class_logic.py
+++ sweet/Class_logic.py
@@ -3,9 +3,11 @@ class SweetShop:
         # Initialize an empty list to store sweet data
         self.sweets = []
 
+
     def add_sweet(self, id, name, category, price, quantity):
         
         #Add a new sweet to the shop. All fields are required.
+        
         if not id or not name or not category or price is None or quantity is None:
             raise ValueError("Missing required sweet fields.")
 
@@ -16,11 +18,14 @@ class SweetShop:
             "price": price,
             "quantity": quantity
         })
+
+
     def view_sweets(self):
 
         # Return a list of all available sweets.
         return self.sweets
     
+
     def delete_sweet(self, id):
         
         #Delete a sweet by its unique ID.
@@ -31,6 +36,7 @@ class SweetShop:
                 return
         raise ValueError("Sweet not found.")
     
+
     def search_sweets(self, name=None, category=None, min_price=None, max_price=None):
         
         #Search for sweets using optional filters:
@@ -47,6 +53,7 @@ class SweetShop:
             results = [s for s in results if s["price"] <= max_price]
         return results
     
+
     def sort_sweets(self, by="price"):
         
         #Sort sweets by price, name, or quantity.
@@ -55,3 +62,17 @@ class SweetShop:
         if by not in ["price", "name", "quantity"]:
             raise ValueError("Invalid sort field. Choose price, name, or quantity.")
         return sorted(self.sweets, key=lambda x: x[by])
+    
+
+    def purchase_sweet(self, id, quantity):
+        
+        #Purchase a sweet by reducing its quantity.
+        #Raises error if sweet not found or not enough stock.
+        
+        for sweet in self.sweets:
+            if sweet["id"] == id:
+                if sweet["quantity"] < quantity:
+                    raise ValueError("Not enough stock available.")
+                sweet["quantity"] -= quantity
+                return
+        raise ValueError("Sweet not found.")
